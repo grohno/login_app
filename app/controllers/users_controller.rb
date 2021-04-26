22a58a6888af8a90d93e4535aaaf7e7cb32b5b48
yuki ohno <jeanadler33@gmail.com>
2021-04-26 08:17:32 +0000

    追加課題を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @favorites = @user.favorites
+    @favorited_total_counts = Favorite.joins(:blog).where(blogs: {user_id: current_user.id}).count
   end
   private
   def user_params
